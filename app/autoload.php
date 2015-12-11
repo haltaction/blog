2c87d53874d7309c23630cf590f695fd90ad85f6
Fialkovskiy <haltaction@gmail.com>
2015-12-11 19:53:26 +0200

    Unfinished, added & setup FOSU

--- app/autoload.php
+++ app/autoload.php
@@ -2,6 +2,7 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 
 error_reporting(error_reporting() & ~E_USER_DEPRECATED);
 
@@ -11,5 +12,6 @@ error_reporting(error_reporting() & ~E_USER_DEPRECATED);
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+AnnotationDriver::registerAnnotationClasses();
 
 return $loader;
