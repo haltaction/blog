700e070ffb599437cec68ea7ee5b547401a603b4
Fialkovskiy <haltaction@gmail.com>
2015-12-23 10:26:01 +0200

    Styling form, translate for, message, added tag trim.

--- src/BlogBundle/TagService.php
+++ src/BlogBundle/TagService.php
@@ -3,21 +3,37 @@
 namespace BlogBundle;
 
 use BlogBundle\Document\Tag;
+use BlogBundle\Document\TagRepository;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\DocumentRepository;
 
 class TagService
 {
+    /**
+     * @var TagRepository
+     */
     protected $tagRepository;
 
+    /**
+     * @var DocumentManager
+     */
     protected $documentManager;
 
+    /**
+     * @param DocumentRepository $tagRepository
+     * @param DocumentManager $documentManager
+     */
     public function __construct(DocumentRepository $tagRepository, DocumentManager $documentManager)
     {
         $this->tagRepository = $tagRepository;
         $this->documentManager = $documentManager;
     }
 
+    /**
+     * @param string $tagsString
+     *
+     * @return array
+     */
     public function getTagIds($tagsString = '')
     {
         $tags = explode(',', $tagsString);
@@ -25,14 +41,15 @@ class TagService
 
         if (is_array($tags)) {
             foreach ($tags as $tag) {
+                $tag = trim($tag);
                 $tagDocument = $this->tagRepository->getTagByName($tag);
 
                 if (empty($tagDocument)) {
                     $tagDocument = new Tag();
                     $tagDocument->setName($tag);
                     $this->documentManager->persist($tagDocument);
-
                 }
+
                 $tagDocument->incrementNumberArticles();
                 $tagsHash[$tag] = $tagDocument->getId();
             }
@@ -40,4 +57,4 @@ class TagService
 
         return $tagsHash;
     }
-} 
+}
