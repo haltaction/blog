5507366dc719a2e6bf8f93e1e5881bc6d06fab13
Fialkovskiy <haltaction@gmail.com>
2015-12-25 15:09:52 +0200

    Added twig sentences filter, vizual fix

---
+++ src/BlogBundle/Twig/TwigExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace BlogBundle\Twig;
+
+class TwigExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('sentencesLimit', array($this, 'sentencesLimit'))
+        );
+    }
+
+    public function sentencesLimit($string, $sentencesNumber = 1)
+    {
+        $sentences = preg_split('/(?<=[.?!])\s+(?=[a-z])/i', $string);
+        $sentences = array_slice($sentences, 0, $sentencesNumber);
+        $result = implode(' ', $sentences);
+        return $result;
+    }
+
+    public function getName()
+    {
+        return 'twig_extension';
+    }
+}
