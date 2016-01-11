46e0c76e169d5d21bc385d985a04ac476945fef2
Fialkovskiy <haltaction@gmail.com>
2016-01-11 11:27:45 +0200

    Finished comments fixtures

---
+++ src/BlogBundle/DataFixtures/ODM/BlogFixturesLoader.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace BlogBundle\DataFixtures\ODM;
+
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Hautelook\AliceBundle\Doctrine\DataFixtures\AbstractLoader;
+
+class BlogFixturesLoader extends AbstractLoader implements DependentFixtureInterface
+{
+    /**
+     * Returns an array of file paths to fixtures. File paths can be relatives, specified with the `@Bundlename`
+     * notation or being SplFileInfo instances.
+     *
+     * @return string[]|\SplFileInfo[]
+     */
+    public function getFixtures()
+    {
+        return [
+            __DIR__.'/tag.yml',
+            __DIR__.'/comment.yml',
+            __DIR__.'/article.yml',
+        ];
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    function getDependencies()
+    {
+        return [
+            'UserBundle\DataFixtures\ODM\UserFixturesLoader'
+        ];
+    }
+}
---
+++ src/UserBundle/DataFixtures/ODM/UserFixturesLoader.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace UserBundle\DataFixtures\ODM;
+
+class UserFixturesLoader
+{
+//    /**
+//     * Returns an array of file paths to fixtures. File paths can be relatives, specified with the `@Bundlename`
+//     * notation or being SplFileInfo instances.
+//     *
+//     * @return string[]|\SplFileInfo[]
+//     */
+//    public function getFixtures()
+//    {
+//        return [
+//            __DIR__.'/user.yml',
+//        ];
+//    }
+}
